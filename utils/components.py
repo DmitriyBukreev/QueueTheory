6e964f52bc6407fe4c9b51f36b85619e37d7eab2
DmitriyBukreev <dmitribukree@yandex.ru>
2019-10-15 22:32:10 +0300

    Initial model implementation

--- utils/components.py
+++ utils/components.py
@@ -1,5 +1,12 @@
 import heapq
 from queue import Queue
+from collections import namedtuple
+from typing import Union, Callable, Iterable, Type
+
+Transaction = namedtuple('Transaction', 'name')
+
+Event = namedtuple('Event', ('transaction', 'outgoing', 'incoming'))
+
 
 class FutureEventsList:
     def __init__(self, first=None):
@@ -12,63 +19,92 @@ class FutureEventsList:
         return heapq.heappop(self._events)
 
 
-class ResourceException(Exception):
-    def __init__(self, res):
-        message = f"Resource {res.name} is already {'busy' if res.busy else 'free'}"
-        super().__init__(message)
+class Generator:
+    def __init__(self, tr_name: str, next, release_law: Callable):
+        self._tr_name = tr_name
+        self._release_law = release_law
+        self._next = next
+
+    def outgoing(self):
+        return self._release_law(), Event(Transaction(self._tr_name), self.outgoing, self._next.incoming)
 
 
+class QueueSet:
+    def __init__(self, queues: Iterable[Queue]):
+        self._queues = queues
+
+    def put(self, item):
+        """ Puts transaction in queue with minimal number of transactions """
+        chosen_queue = min(self._queues, key=lambda x: x.qsize())
+        chosen_queue.put(item)
+
+    def get(self):
+        """ Gets transaction from queue with maximal number of transactions """
+        chosen_queue = max(self._queues, key=lambda x: x.qsize())
+        return chosen_queue.get()
+
+    def empty(self):
+        return all([queue.empty() for queue in self._queues])
+
+    def full(self):
+        return all([queue.full() for queue in self._queues])
+
+
+# TODO Set of Resources as next
 class Resource:
-    def __init__(self, name: str, busy: bool = False):
+    def __init__(self, name: str, queue: Union[Type[Queue], QueueSet, None], next, release_law: Callable):
         self._name = name
-        self._busy = busy
+        self._queue = queue
+        self._next = next
+        self._release_law = release_law
+        self._transaction = None
 
     @property
     def name(self):
         return self._name
 
-    @property
-    def busy(self):
-        return self._busy
+    def _time_and_event(self):
+        return self._release_law(), Event(self._transaction, self.outgoing, self._next.incoming)
 
-    def act(self, action):
-        """ :param action: Handler.FREE or Handler.TAKE """
-        if action == self.busy:
-            raise ResourceException(self)
-        self._busy = action
+    def incoming(self, transaction: Transaction):
+        if self._transaction is None:  # Resource is free
+            if self._queue and not self._queue.empty():  # TODO Should I even check that?
+                self._transaction = self._queue.get()
+                if not self._queue.full():
+                    self._queue.put(transaction)
+            else:
+                self._transaction = transaction
+            return self._time_and_event()
+        elif self._queue and not self._queue.full():
+            self._queue.put(transaction)
 
-
-class Handler:
-    TAKE = True
-    FREE = False
-
-    def __init__(self, action, resource: Resource, queue: Queue = None):
-        self._action = action
-        self._resource = resource
-        self._queue = queue
-
-    def _take_resource(self):
-        if self._resource.busy and self._queue:
-            pass
-
-    def _free_resource(self):
-        if self._resource.busy and self._queue:
-            self._queue.put()
-
-    def __call__(self):
-        if self._action == self.TAKE:
-            self._take_resource()
+    def outgoing(self):
+        if self._queue and not self._queue.empty():
+            self._transaction = self._queue.get()
+            return self._time_and_event()
         else:
-            self._free_resource()
-
-
-class Event:
-    def __init__(self, name: str, handler: Handler):
-        self.name = name
-        self.handler = handler
+            self._transaction = None
+            return None
 
 
 class Model:
-    pass
+    def __init__(self, generators: Iterable[Generator], resources: Iterable[Resource], maxtime: int = 100):
+        self._generators = generators
+        self._resources = resources
+        self._fte = FutureEventsList()
+        self._time = 0
+        self._maxtime = maxtime
 
+    def run(self):
+        for generator in self._generators:
+            time_and_event = generator.outgoing()
+            self._fte.put(*time_and_event)
 
+        while self._time < self._maxtime:
+            self._time, event = self._fte.get()
+            time_and_event = event.outgoing()
+            if time_and_event:
+                o_time, o_event = time_and_event
+                self._fte.put(self._time + o_time, o_event)
+            i_time, i_event = event.incoming(event.transaction)
+            self._fte.put(self._time + i_time, i_event)
