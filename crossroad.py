ed3119a5224616e1bc89b9d43709931a77974d1a
Ateller <tony.tel@yandex.ru>
2019-10-18 18:02:13 +0300

    All roads

--- crossroad.py
+++ crossroad.py
@@ -32,7 +32,7 @@ def new_car():
                 first_car.car = Car(events_list.time)
                 log_event('Машина встала на красный')
         else:
-            Car.count_green_car()
+            stats.count_green_car()
             log_event('Машина проехала свободно')
 
 def car_go():
@@ -52,7 +52,7 @@ def car_go():
         log_event('Машина не успела выехать до красного')
         return
 
-    first_car.car.count_car()
+    stats.count_car(first_car.car)
 
     if not first_car.queue.empty():
         first_car.car = first_car.queue.get()
@@ -80,11 +80,21 @@ def how_many_cars():
 def exp_time(avg):
     return random.expovariate(1.0/avg)
     
+def write_simulation_parameters(file, direction):
+    file.write(f'{direction} новая машина в среднем раз в {Handler.intensity} c.\n')
+    file.write(f'Длительность зеленой фазы светофора - {Crossroad.green_phase_time} с.\n')
+    file.write(f'Длительность красной фазы светофора - {Crossroad.red_phase_time} c.\n')
+    if Crossroad.is_green():
+        file.write(f'Светофор начинает c зеленой фазы\n\n')
+    else:
+        file.write(f'Светофор начинает c красной фазы\n\n')
+        
+    
 class Crossroad:
     GREEN = True
     RED = False
     phase = RED
-    red_phase_time = 20
+    red_phase_time = 10
     green_phase_time = 20
     switch_time = red_phase_time
     
@@ -96,6 +106,14 @@ class Crossroad:
             return Crossroad.green_phase_time
         else:
             return Crossroad.red_phase_time
+            
+    def swap_phases():
+        Crossroad.phase = not Crossroad.phase
+        
+        buffer = Crossroad.green_phase_time
+        Crossroad.green_phase_time = Crossroad.red_phase_time
+        Crossroad.red_phase_time = buffer
+    
     
 class Car:
     delay = 1
@@ -103,55 +121,89 @@ class Car:
     speed = 17 #m/s
     acceleration_time = 5 #s
     
-    counter = 0
-    green_counter = 0
-    sum_time = 0
-    
     def __init__(self, time):
         self.time = time
-        
-    def count_green_car():
-        Car.green_counter += 1
-        
-    def count_car(self):
-        Car.sum_time += (events_list.time - self.time)
-        Car.counter += 1
-        
-    def get_avg_queue_time():
-        return Car.sum_time/Car.counter
-        
-    def get_avg_time_with_greens():
-        return Car.sum_time/(Car.counter + Car.green_counter)
-    
-    def get_avg_time_with_acceleration():
-        return (Car.sum_time + (2 * Car.acceleration_time * Car.counter))/(Car.counter + Car.green_counter)
-        
 
-Handler.intensity = 4
+        
+class Stats:
+    def __init__(self):
+        self.counter = 0
+        self.green_counter = 0
+        self.sum_time = 0
+    
+    def count_green_car(self):
+        self.green_counter += 1
+        
+    def count_car(self, car):
+        self.sum_time += (events_list.time - car.time)
+        self.counter += 1
+        
+    def get_avg_queue_time(self):
+        return self.sum_time/self.counter
+        
+    def get_avg_time_with_greens(self):
+        return self.sum_time/(self.counter + self.green_counter)
+    
+    def get_avg_time_with_acceleration(self):
+        return (self.sum_time + (2 * Car.acceleration_time * self.counter))/(self.counter + self.green_counter)
+        
+    def write_stats(self, file, direction):
+        file.write(f'{direction} проехало машин: {self.counter}\n')
+        file.write(f'Среднее время нахождения машины в очереди равно {self.get_avg_queue_time()}\n')
+        file.write(f'С учетом проехавших свободно {self.get_avg_time_with_greens()}\n')
+        file.write(f'Средние затраты на проезд перекрестка (торможение, очередь и разгон) {self.get_avg_time_with_acceleration()}\n\n')
+
+intensities = [('С севера', 1),('C запада', 2),('С юга', 3),('С востока', 4)]
+all_stats = []
 
 log = open('log.txt', 'w')
+results = open('results.txt', 'w')
 
-for i in range(1000):
+for intensity in intensities:
 
-    first_car = Resource('First car')
-    first_car.distance_from_cross = 0
-    first_car.queue = Queue()
-
-    phase_switch_handler = Handler(switch_phase)
-    new_car_handler = Handler(new_car)
-    car_go_handler = Handler(car_go)
-
-    events_list = FutureEventsList((Crossroad.switch_time, Event('Phase switch', phase_switch_handler)))
-    events_list.put(exp_time(Handler.intensity), Event('New car', new_car_handler))
-
-    while events_list.time < 1000:
-        events_list.get().handler._function()
-
-    log.write('\nМашин:' + str(Car.counter) + '\n')
-    log.write('Среднее время нахождения машины в очереди равно ' + str(Car.get_avg_queue_time()) + '\n')
-    log.write('С учетом проехавших свободно ' + str(Car.get_avg_time_with_greens()) + '\n')
-    log.write('Средние затраты на проезд перекрестка (торможение, очередь и разгон) ' + str(Car.get_avg_time_with_acceleration()) + '\n\n')
+    Handler.intensity = intensity[1]
+    stats = Stats()
     
-    Crossroad.switch_time = Crossroad.phase_time()
+    write_simulation_parameters(log, intensity[0])
+    write_simulation_parameters(results, intensity[0])
 
-log.close
+    for i in range(250):
+
+        first_car = Resource('First car')
+        first_car.distance_from_cross = 0
+        first_car.queue = Queue()
+
+        phase_switch_handler = Handler(switch_phase)
+        new_car_handler = Handler(new_car)
+        car_go_handler = Handler(car_go)
+
+        events_list = FutureEventsList((Crossroad.switch_time, Event('Phase switch', phase_switch_handler)))
+        events_list.put(exp_time(Handler.intensity), Event('New car', new_car_handler))
+
+        while True:
+            event = events_list.get()
+            if events_list.time <= 1000:
+                event.handler._function()
+            else:
+                break
+
+        log.write('\n')
+        stats.write_stats(log, intensity[0])
+        
+        Crossroad.switch_time = Crossroad.phase_time()
+        
+    Crossroad.swap_phases()
+    stats.write_stats(results, intensity[0])
+    all_stats.append(stats)
+    
+result_stats = Stats()
+for stats in all_stats:
+    result_stats.counter += stats.counter
+    result_stats.green_counter += stats.green_counter
+    result_stats.sum_time += stats.sum_time
+
+result_stats.write_stats(log, 'Всего через перекресток')
+result_stats.write_stats(results, 'Всего через перекресток')
+
+results.close()
+log.close()
