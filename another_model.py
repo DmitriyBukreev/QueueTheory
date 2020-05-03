b8bf25a3bab657b8a44998201d25097db9e15632
Ateller <tony.tel@yandex.ru>
2020-05-03 22:39:11 +0300

    stats

--- another_model.py
+++ another_model.py
@@ -4,6 +4,7 @@ from queue import Queue
 
 def arrival(arrival_where):
     type = 0 if arrival_where[-1] == 'e' else 1;
+    stats.count_arrived(type)
     qsize = q_size(type)
 
     if(resource_one.queues[0].empty() and resource_one.queues[1].empty()):
@@ -11,28 +12,33 @@ def arrival(arrival_where):
         events_list.put(exp_time(intensities[1]), Event('Release one', release_one_handler))
         log_event(f'Заявка типа {type + 1} заходит на устройство 1, занимая его', 0)
         resource_one.queues[type].put(Transact(events_list.time, type))
+        stats.start_load(0)
     else:
         log_event(f'Заявка типа {type + 1} пришла на устройство 1, а оно занято', qsize)
         if (qsize < 4):
             resource_one.queues[type].put(Transact(events_list.time, type))
             log_note('в очереди есть место, заявка встает в очередь', qsize + 1)
         else:
+            stats.count_dropped(type, type, events_list.time)
             log_note('в очереди нету места, заявка сбрасывается', -1)
 
     events_list.put(exp_time(intensities[type]), Event(arrival_where, arrival_handler))
 
 def release_one(release_what):
     transact = resource_one.queues[resource_one.which].get()
+    stats.count_load(0)
     log_event(f'Заявка типа {resource_one.which + 1} покинула устройство 1', resource_one.queues[resource_one.which].qsize())
 
     if(not resource_one.queues[0].empty()):
         resource_one.which = 0
         events_list.put(exp_time(intensities[2]), Event('Release one', release_one_handler))
         log_note('заявка из очереди 1 занимает устройство 1', resource_one.queues[0].qsize() - 1)
+        stats.start_load(0)
     elif(not resource_one.queues[1].empty()):
         resource_one.which = 1
         events_list.put(exp_time(intensities[2]), Event('Release one', release_one_handler))
         log_note('заявка из очереди 2 занимает устройство 1', resource_one.queues[1].qsize() - 1)
+        stats.start_load(0)
     else:
         log_note('обе очереди пусты, устройство 1 никто не занимает', -1)
 
@@ -42,24 +48,89 @@ def release_one(release_what):
             queue_three.put(transact)
             log_note('в очереди есть место, заявка встает в очередь', queue_three.qsize() - 1)
         else:
+            stats.count_dropped(2, transact.type, transact.time)
             log_note('в очереди нету места, заявка сбрасывается', -1)
     else:
         events_list.put(exp_time(intensities[3]), Event('Release two', release_two_handler))
         log_note('покинувшая заявка заходит на устройство 2, занимая его', 0)
+        stats.start_load(1)
         queue_three.put(transact)
 
 def release_two(release_what):
-    queue_three.get()
+    transact = queue_three.get()
+    stats.count_load(1)
 
     if(not queue_three.empty()):
         events_list.put(exp_time(intensities[3]), Event('Release two', release_two_handler))
         log_event('Заявка уходит с устройства 2, ee место занимает следующая', queue_three.qsize() - 1)
+        stats.start_load(1)
     else:
         log_event('Заявка уходит с устройства 2 и покидает модель', 0)
 
+    stats.count_passed(transact.type, transact.time)
+
 class Stats:
     def __init__(self):
-        pass
+        self.arrived = [0,0]
+        self.dropped = [0,0,0]
+        self.passed = [0,0]
+        self.load_starts = [0.0, 0.0]
+        self.load = [0.0, 0.0]
+        self.time_counter = [0.0, 0.0]
+        self.dropped_time_counter = [0.0, 0.0]
+        self.count_in_model = [0,0]
+        self.count_counter = [0.0, 0.0]
+        self.last_count_change_time = [0,0]
+
+    def count_arrived(self, type):
+        self.count_count(type)
+        self.arrived[type] += 1
+        self.count_in_model[type] += 1
+        self.last_count_change_time[type] = events_list.time
+
+    def count_dropped(self, queue_number, type, time):
+        self.count_count(type)
+        self.dropped[queue_number] += 1
+        self.dropped_time_counter[type] += events_list.time - time
+        self.count_in_model[type] -= 1
+        self.last_count_change_time[type] = events_list.time
+
+    def count_passed(self, type, time):
+        self.count_count(type)
+        self.time_counter[type] += events_list.time - time
+        self.count_in_model[type] -= 1
+        self.last_count_change_time[type] = events_list.time
+
+    def start_load(self, res_number):
+        self.load_starts[res_number] = events_list.time
+
+    def count_load(self, res_number):
+        self.load[res_number] += events_list.time - self.load_starts[res_number]
+
+    def count_count(self, type):
+        self.count_counter[type] += self.count_in_model[type] * (events_list.time - self.last_count_change_time[type])
+
+    def write_stats(self, file):
+        file.write(f'\nВ модель поступило:\n')
+        file.write(f'\t- {self.arrived[0]} заявок типа 1\n')
+        file.write(f'\t- {self.arrived[1]} заявок типа 2\n')
+        file.write(f'\t- {self.arrived[0] + self.arrived[1]} заявок всего\n')
+        file.write(f'\nБыло потеряно:\n')
+        file.write(f'\t- {self.dropped[0]} заявок на входе очереди 1\n')
+        file.write(f'\t- {self.dropped[1]} заявок на входе очереди 2\n')
+        file.write(f'\t- {self.dropped[2]} заявок на входе очереди 3\n')
+        file.write(f'\t- {self.dropped[0] + self.dropped[1] + self.dropped[2]} заявок всего\n')
+        file.write(f'\nБыло загружено:\n')
+        file.write(f'\t- На {((self.load[0] / 180) * 100):2.2f}% устройство 1\n')
+        file.write(f'\t- На {((self.load[1] / 180) * 100):2.2f}% устройство 2\n')
+        file.write(f'\nСреднее время обслуживания составило:\n')
+        file.write(f'\t- {self.time_counter[0]/(self.arrived[0] - self.dropped[0]):2.2f} для заявок типа 1, {(self.time_counter[0] + self.dropped_time_counter[0])/self.arrived[0]:2.2f} c учетом сброшенных\n')
+        file.write(f'\t- {self.time_counter[1]/(self.arrived[1] - self.dropped[1]):2.2f} для заявок типа 2, {(self.time_counter[1] + self.dropped_time_counter[1])/self.arrived[1]:2.2f} c учетом сброшенных\n')
+        file.write(f'\t- {(self.time_counter[0] + self.time_counter[1])/(self.arrived[0] - self.dropped[0] + self.arrived[1] - self.dropped[1]):2.2f} для всех заявок, {(self.time_counter[0] + self.time_counter[1] + self.dropped_time_counter[0] + self.dropped_time_counter[1])/(self.arrived[0] + self.arrived[1]):2.2f} с учетом сброшенных\n')
+        file.write(f'\nВ среднем в модели одновременно находилось:\n')
+        file.write(f'\t- {self.count_counter[0]/180:2.2f} заявок типа 1\n')
+        file.write(f'\t- {self.count_counter[1]/180:2.2f} заявок типа 2\n')
+        file.write(f'\t- {(self.count_counter[0] + self.count_counter[1])/180:2.2f} заявок всего\n')
 
 class Transact:
     def __init__(self, time, type):
@@ -110,6 +181,8 @@ while True:
         event.handler._function(event.name)
     else:
         break
+
+stats.write_stats(results)
 
 log.close()
 results.close()
